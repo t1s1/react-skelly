8d55a30e3c4c8ea1f3f2dc8473f6069cc48824c8
t1s1 <tedesi@gmail.com>
2016-02-22 16:00:34 +0000

    genericized

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,14 @@
 var React = require("react");
 var ListItem = require("./ListItem.jsx");
 
-var ingredients = [{"id":1,"text":"ham"},{"id":2,"text":"cheese"},{"id":3,"text":"potatoes"},]
+var items = [{"id":1,"textProp":"first"},{"id":2,"textProp":"second"},{"id":3,"textProp":"third"},]
 
 var List = React.createClass({
     
     render: function() {
-        var listItems = ingredients.map(function(item) {
-            return <ListItem key={item.id} ingredient={item.text} />;
-         });
+        var listItems = items.map(function(item) {
+            return <ListItem key={item.id} text={item.textProp} />;
+         }); 
          
         return (<ul>{listItems}</ul>);
     }
